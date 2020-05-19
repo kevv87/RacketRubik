23b3df6f794d11219bd7744f8a966761fdf26267
Kevin <kev_sala@outlook.com>
2020-05-19 15:09:20 +0000

    Cubo personalizado

--- proyecto.rkt
+++ proyecto.rkt
@@ -1934,7 +1934,7 @@ Funciones principales
 (cond ((> (round X) 1)
   (cond ((null? Cubo)
 	  ( big-bang3d (list (caras_moviles X (format_x X (cubo_standard X) 1 1)) X '() #|(traducirMovs Movs)|# '()  "x" '()  ) #:on-draw on-draw #:on-frame on-frame #:frame-delay (/ 1000 140) #:on-mouse on-mouse))   ;Si no ingresa un cubo, genera un cubo NxN estandar
-	(else (aplic_movs X (identificar 'x (caras_moviles X (format_x X Cubo 1 1))) Movs))))
+	(else ( big-bang3d (list (caras_moviles X (format_x X Cubo 1 1)) X '() #|(traducirMovs Movs)|# '()  "x" '()  ) #:on-draw on-draw #:on-frame on-frame #:frame-delay (/ 1000 140) #:on-mouse on-mouse))))
 (else #f)))
 (RSP 3 '() '("C3A"))
 
